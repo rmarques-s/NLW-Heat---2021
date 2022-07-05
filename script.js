4889e58ba713eeeccf6158b3d22f6d2ce3a6c9a9
Rafaela Ribeiro Marques dos Santos <65922146+rmarques-s@users.noreply.github.com>
2022-07-05 00:13:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function showMeSomething(texto) {
+  texto = 'NLW - 2021'
+  alert(texto)
+}
+
+const links = {
+  github: 'rmarques-s',
+  youtube: 'UCuwAxRLD7pnvN-9AqHQ0hHQ',
+  facebook: 'rafaelasantos.santos.902',
+  instagram: '_rmmarques',
+  twitter: 'rafaelam4rques'
+}
+
+showMeSomething()
